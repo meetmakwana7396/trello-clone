0fce222ae16aae29c947f867b013b195f31d3990
Meetmakwana7396 <makwnameet7301@gmail.com>
2023-12-09 01:20:50 +0530

    integrated prisma

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,7 +1,32 @@
+import { createBoard } from "@/actions/create-board";
+import { Button } from "@/components/ui/button";
+import { db } from "@/lib/db";
 import React from "react";
 
-const OrganizationPage = () => {
-  return <div>Organization Page </div>;
+const OrganizationPage = async () => {
+  const boards = await db.board.findMany();
+  return (
+    <div className="flex flex-col space-y-4">
+      <form action={createBoard}>
+        <input
+          type="text"
+          id="title"
+          name="title"
+          placeholder="Enter a board title"
+          className="p-1 border"
+          required
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+      <div className="space-y-2">
+        {boards.map((board) => (
+          <div className="" key={board.id}>
+            Board title:{board.title}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default OrganizationPage;
